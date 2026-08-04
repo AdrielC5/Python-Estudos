7eb970d3bd7a911c205dea4cedd577fff34f250c
Adriel_Carvalho <ac2perfil@gmail.com>
2026-08-04 17:06:51 -0300

    Adicionando exercício de join()

--- 8_Strings_na_Prática/main.py
+++ 8_Strings_na_Prática/main.py
@@ -1,5 +1,15 @@
-frase = input("Digite uma frase qualquer: ")
-palavras = frase.split()
+def exerciciosplit():
+    frase = input("Digite uma frase qualquer: ")
+    palavras = frase.split()
 
-print(f"A frase tem {len(palavras)} palavras")
-print(f"A última palavra é: {palavras[-1]}")
+    print(f"A frase tem {len(palavras)} palavras")
+    print(f"A última palavra é: {palavras[-1]}")
+
+def exerciciojoin():
+    frase = input("Digite uma frase aleatória: ")
+    palavras = frase.split()
+    frase_nova = "-".join(palavras)
+
+    print(f"{frase_nova}")
+
+exerciciojoin()
