47d0216850a922a94ac87e128b5b7e5a83618c87
Adriel_Carvalho <ac2perfil@gmail.com>
2026-08-04 16:47:19 -0300

    Fazendo exercício de split()

--- 8_Strings_na_Prática/main.py
+++ 8_Strings_na_Prática/main.py
@@ -1,1 +1,5 @@
-print("Teste")
+frase = input("Digite uma frase qualquer: ")
+palavras = frase.split()
+
+print(f"A frase tem {len(palavras)} palavras")
+print(f"A última palavra é: {palavras[-1]}")
