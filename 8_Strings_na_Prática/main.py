9c17f2e351cddeaf583e852d78b276dfe1ca4ff6
Adriel_Carvalho <ac2perfil@gmail.com>
2026-08-04 22:17:09 -0300

    Comentários para ajudar a entender o código

--- 8_Strings_na_Prática/main.py
+++ 8_Strings_na_Prática/main.py
@@ -1,6 +1,6 @@
 def exerciciosplit():
     frase = input("Digite uma frase qualquer: ")
-    palavras = frase.split()
+    palavras = frase.split() #split faz a frase digitada ser separada em palavras únicas
 
     print(f"A frase tem {len(palavras)} palavras")
     print(f"A última palavra é: {palavras[-1]}")
@@ -10,14 +10,12 @@ def exerciciosplit():
 def exerciciojoin():
     frase = input("Digite uma frase aleatória: ")
     palavras = frase.split()
-    frase_nova = "-".join(palavras)
+    frase_nova = "-".join(palavras) #join une palavras em uma unica frase
 
     print(f"{frase_nova}")
 
 ##################################################
 
 def exercicio_upper_lower_strip():
-    email = input("Digite seu e-mail: ").lower().strip()
+    email = input("Digite seu e-mail: ").lower().strip() #lower = minuscula | strip = deleta espaços em branco
     print (f"Bem vindo, seu email é: {email}")
-
-exercicio_upper_lower_strip()
