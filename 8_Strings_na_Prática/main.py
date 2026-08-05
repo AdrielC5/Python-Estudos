a354ccc33671119457c444b39a2fe5200ecee4ab
Adriel_Carvalho <ac2perfil@gmail.com>
2026-08-04 22:38:17 -0300

    Atualizando o exercicio de Fatiamento e Comentários

--- 8_Strings_na_Prática/main.py
+++ 8_Strings_na_Prática/main.py
@@ -20,10 +20,15 @@ def exercicio_upper_lower_strip():
     email = input("Digite seu e-mail: ").lower().strip() #lower = minuscula | strip = deleta espaços em branco
     print (f"Bem vindo, seu email é: {email}")
 
+##################################################
+
 def exerciciofatiamento():
     palavra = input("Digite uma palavra: ")
     print(palavra[:3]) #puxa as três PRIMEIRAS letras da palavra que o usuário digitou
-    print(palavra[3:]) #puxa as três ÚLTIMAS letras da palavra que o usuário digitou
+    print(palavra[3:]) #pega da terceira POSIÇÃO da palavra que o usuário digitou em diante
+    print(palavra[-3:]) #puxa as três ÚLTIMAS letras da palavra que o usuário digitou por conta do -
     print(palavra[: :-1]) #faz a palavra digitada pelo usuário se INVERTER, ficando de trás pra frente
+
+# LEMBRANDO QUE A CONTAGEM COMEÇA EM 0 E NÃO EM 1 !!!!!!
 
 exerciciofatiamento()
