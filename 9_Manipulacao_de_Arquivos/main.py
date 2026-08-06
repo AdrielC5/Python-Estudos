9285282cd0d94db205d1803fc5770be0d21f4785
Adriel_Carvalho <ac2perfil@gmail.com>
2026-08-06 11:58:11 -0300

    Aprendendo como funciona um arquivo .CSV e fazendo um exercicio importando a biblioteca

--- 9_Manipulacao_de_Arquivos/main.py
+++ 9_Manipulacao_de_Arquivos/main.py
@@ -8,4 +8,27 @@ def exercicioarq1():
         for linha in arquivo:
             print(linha)
 
-exercicioarq1()
+def exercicioarq2():
+    import csv
+
+    nome = input("Olá, digite seu nome por favor: ")
+
+    while True:
+        try:
+            idade = int(input("Agora, digite sua idade: "))
+            break
+        except ValueError:
+            print("Digite um número válido!")
+
+    cidade = input("Agora, digite a cidade onde mora: ")
+
+    with open("pessoas.csv", "a", encoding="utf-8") as arquivo:
+        escritor = csv.writer(arquivo)
+        escritor.writerow([nome, idade, cidade])
+
+    with open("pessoas.csv", "r", encoding="utf-8") as arquivo:
+        leitor = csv.reader(arquivo)
+        for linha in leitor:
+            print(linha)
+
+exercicioarq2()
